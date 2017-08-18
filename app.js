8766d81300e4da4d791da5babe5ac93dd0a321e1
Peeraphat Thongfueng <peeraphat.thong@gmail.com>
2017-08-18 23:11:46 +0700

    app.js

--- app.js
+++ app.js
@@ -3,55 +3,41 @@ mongoose.connect('mongodb://localhost:27017/test', {
     useMongoClient: true
 })
 mongoose.Promise = global.Promise
-const { Schema } = mongoose
-
-const personSchema = Schema({
-    _id : Number,
-    name: String,
-    age : Number,
-    stories: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Story'
-    }]
-})
-
-const storySchema = Schema({
-    _creator: {
-        type: Number, ref: 'Person'
-    },
-    title: String,
-    fans : [{
-        type: Number, ref: 'Person'
-    }]
-})
-
-const Story = mongoose.model('Story', storySchema)
-const Person = mongoose.model('Person', personSchema)
+const { Person, Story } = require('./models')
 
 
-const aaron = new Person({
-    _id: 2,
+var aaron = new Person({
+    _id: 1,
     name: 'Aaron',
     age: 100
 })
+const story1 = new Story({
+    title: "Once upon a timex.",
+    _creator: aaron._id
+})
 
+aaron.stories.push(story1)
 aaron.save((err) => {
-    if(err) console.error('err', err)
-    console.log('id', aaron._id)
-    const story1 = new Story({
-        title: "Once upon a times.",
-        _creator: aaron._id
-    })
-
+    if(err) console.error(err)
     story1.save((err) => {
         if(err) console.error('err', err)
-        console.log('Awesome')
     })
 })
 
-Story.findOne({ title: 'Once upon a times.'}).
-populate('_creator').
-exec((err, story) => {
-    if(err) console.error('err', err)
-    console.log('The creator is %s', story._creator.name)
-})
+/* Story.findOne({ title: 'Once upon a timex.'}).populate('_creator').exec().then(story => {
+    console.log(story)
+    // console.log(`The creator is ${story._creator.name}`)
+}).catch(err => console.error('err', err)) */
+
+/* Story.findOne({ title: 'Once upon a timex.' }).exec().then(story => {
+    story._creator = aaron
+    console.log(story._creator.name)
+}).catch(err => console.error('err', err)) */
+/* 
+Story.findOne({ title: /timex/i }).populate('_creator', 'name').exec().then(story => {
+    console.log(story)
+}).catch(err => console.error('err', err)) */
+
+Person.findOne({ name: 'Aaron' }).sort({_id:-1}).populate('stories').exec().then(person => {
+    console.log(person)
+}).catch(err => console.error('err', err))
